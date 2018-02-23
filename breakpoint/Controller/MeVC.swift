88390eabf0a8c59477caa14298e8f67f9c79ec3b
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-22 22:43:56 -0500

    Set up profile vc

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Ricardo Herrera Petit on 2/22/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        //sign out code
+    }
+    
+}
