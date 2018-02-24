e4eed9f8b88792a7a2e6ce1b03042ae5ea074cfc
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-23 22:26:22 -0500

    Binding keyboard to view

---
+++ breakpoint/Extensions/UIViewExt.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewExt.swift
+//  breakpoint
+//
+//  Created by Ricardo Herrera Petit on 2/23/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let beginningFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endFrame.origin.y - beginningFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+        
+        
+        
+    }
+}
